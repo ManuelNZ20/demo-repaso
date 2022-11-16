c32d1bc9995bbeb8472b5ce13821ce6ca4a4ae46
Navarro Zeta Manuel Walter <96985379+ManuelNZ20@users.noreply.github.com>
2022-11-15 21:12:08 -0500

    Create Persona.java
    mi clase persona

---
+++ Persona.java
@@ -0,0 +1,13 @@
+package ejemplo;
+
+public Persona{
+ String nombre;
+ String apellido;
+ public Persona(String nombre,String apellido){
+ this.nombre = nombre;
+this.apellido = apellido;
+  }
+  
+  
+
+}
