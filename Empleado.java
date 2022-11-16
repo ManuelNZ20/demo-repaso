b94cb3626ca72c8a8b4fedddd615fbcacc16e0bb
Navarro Zeta Manuel Walter <96985379+ManuelNZ20@users.noreply.github.com>
2022-11-15 21:25:00 -0500

    Create Empleado.java
    Se creo la clase empleado

---
+++ Empleado.java
@@ -0,0 +1,13 @@
+package ejemplo;
+
+public class Empleado extends Persona{
+	
+	private float sueldo;
+	
+	public(String nombre,String apellido,float sueldo){
+		super(nombre,apellido);
+		this.sueldo = sueldo;
+	}
+	
+	
+}
